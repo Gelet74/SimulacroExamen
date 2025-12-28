34bcece028e52b50200435720f042f0f8b9d9429
Gelet74 <sanchis74@gmail,com>
2025-12-28 20:17:26 +0100

    28/12 20:20

---
+++ app/src/main/java/com/example/simulacroexamen/conexion/SimulacroServicioApi.kt
@@ -0,0 +1,12 @@
+package com.example.simulacroexamen.conexion
+
+import com.example.ud6_ejemplo3.modelo.Trabajador
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
+
+interface SimulacroServicioApi {
+}
